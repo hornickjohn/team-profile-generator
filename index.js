1bea9cd93d71e2a1bf799c8f8523838d681f9cd1
John Hornick <jhornick@live.com>
2023-01-24 14:04:31 -0500

    HTML generation added.

--- index.js
+++ index.js
@@ -3,41 +3,71 @@ const Engineer = require('./lib/engineer');
 const Intern = require('./lib/intern');
 const Manager = require('./lib/manager');
 const inquirer = require('inquirer');
+const fileStream = require('fs');
 
 let team = [];
 
 const RequireAnswer = input => input.trim();
 
+//returns a formatted string literal with HTML contents for a web page representing current team
 function CreateHTML() {
-    console.log(team);
+    let htmlOutput = ``;
+    //create top part of generic html page
+    htmlOutput += `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./style.css">
+    <title>My Team</title>
+</head>
+<body>
+    <header>
+        <h1>Team Profiles</h1>
+    </header>
+    <main>
+`;
+
+    //pass each employee into a function that generates a card displaying them
+    for(let i = 0; i < team.length; i++) {
+        team[i].generateHTMLCard();
+    }
+
+    //create bottom part of generic html page
+    htmlOutput += `
+    </main>
+</body>
+</html>`;
+    return htmlOutput;
 }
 
+//prompt user with sequence of questions getting data for a new employee to add to our data
 function GetEmployeeInfo(employeetype) {
-    //default values for generic employee
+    //default questions for generic employee
+    let questions = [
+        { name:'name', message:'Name:', validation:RequireAnswer },
+        { name:'id', message:'Employee ID:', validation:RequireAnswer },
+        { name:'email', message:'Email Address:', validation:RequireAnswer }
+    ];
     let str = 'employee';
-    let lastQ = {};
 
-    //set question and prompt based on employee type
+    //set final question question and prompt based on employee type
     if(employeetype === 'manager') {
         str = 'team\'s manager';
-        lastQ = { name:'office', message:'Office Number:', validation:RequireAnswer };
+        questions.push({ name:'office', message:'Office Number:', validation:RequireAnswer });
     } else if(employeetype === 'engineer') {
         str = 'engineer';
-        lastQ = { name:'github', message:'Github Username:', validation:RequireAnswer };
+        questions.push({ name:'github', message:'Github Username:', validation:RequireAnswer });
     } else if(employeetype === 'intern') {
         str = 'intern';
-        lastQ = { name:'school', message:'School:', validation:RequireAnswer };
+        questions.push({ name:'school', message:'School:', validation:RequireAnswer });
     }
 
     //query user for information
     console.log('Please provide information about the ' + str + '.');
-    inquirer.prompt([
-        //questions
-        { name:'name', message:'Name:', validation:RequireAnswer },
-        { name:'id', message:'Employee ID:', validation:RequireAnswer },
-        { name:'email', message:'Email Address:', validation:RequireAnswer },
-        lastQ
-    ])
+    inquirer.prompt(questions)
     .then((answers) => {
         //create new object for the employee based on employee type
         if(employeetype === 'manager') {
@@ -58,20 +88,23 @@ function GetEmployeeInfo(employeetype) {
     });
 }
 
+//prompt user with a menu offering them to add an engineer, add an intern, or exit the application
 function DisplayMenu() {
     console.log('Would you like to add a team member?');
     inquirer.prompt([
-        //questions
-        { name:'selection', message:'Would you like to add a team member?', type:'list', choices:['Add an Engineer', 'Add an Intern', 'Team Finished!'] }
+        { name:'selection', message:'Would you like to add a team member?', type:'list', choices:['Add an Engineer', 'Add an Intern', 'Nope, Team Finished!'] }
     ])
     .then((answers) => {
+        //get what user wants to do next, and go there
         if(answers.selection === 'Add an Engineer') {
             GetEmployeeInfo('engineer');
         } else if(answers.selection === 'Add an Intern') {
             GetEmployeeInfo('intern');
         } else {
             //user has selected no more input, wrap up
-            CreateHTML();
+            fileStream.writeFile('teamprofiles.html',CreateHTML(),function(err) {
+                throw err;
+            });
         }
     })
     .catch((error) => {
@@ -80,8 +113,9 @@ function DisplayMenu() {
 }
 
 function init() {
-    //start off getting manager info, menus will self-continue from there
+    //start off getting manager info, menus will recursively continue from there
     GetEmployeeInfo('manager');
 }
 
+//begin CLI application
 init();
